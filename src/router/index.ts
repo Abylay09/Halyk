f8c759bcfe2687ad4f980f05077758a195258d84
Arkinov Abylay <arkinovabylay@gmail.com>
2023-03-06 15:05:30 +0600

    before deploy

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,16 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import AboutView from '../views/AboutView.vue'
 import AuthView from '@/views/AuthView.vue'
 import DocumentView from '@/views/DocumentView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: HomeView
-    // },
     {
       path: '/',
       name: 'auth',
@@ -19,14 +12,6 @@ const router = createRouter({
         Auth: false
       }
     },
-    // {
-    //   path: '/auth',
-    //   name: 'auth',
-    //   component: AuthView,
-    //   meta: {
-    //     Auth: false
-    //   }
-    // },
     {
       path: '/document',
       name: 'document',
